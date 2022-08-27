3a9ed5a5f1c36ed01d06193f6b8e027b2dfee428
Noble_Draconian <Binarybyte.DEV@gmail.com>
2022-08-27 16:39:40 -0400

    Implement initial level loading logic

--- src/Client/Controllers/LevelController/init.lua
+++ src/Client/Controllers/LevelController/init.lua
@@ -6,12 +6,38 @@
 
 local LevelController = {}
 
+---------------------
+-- Roblox Services --
+---------------------
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local CollectionService = game:GetService("CollectionService")
+local Players = game:GetService("Players")
+
+------------------
+-- Dependencies --
+------------------
+local LevelService;
+local AvatarController;
+local LightingController;
+
+-------------
+-- Defines --
+-------------
+local LocalPlayer = Players.LocalPlayer
+local LevelConfigs = ReplicatedStorage.Modules.LevelConfigs
+
+local LevelMusic_Sound = Instance.new('Sound')
+LevelMusic_Sound.Parent = script
+LevelMusic_Sound.Looped = true
+
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- @Name : Init
 -- @Description : Called when the controller module is first loaded.
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 function LevelController:Init()
 	self:DebugLog("[Level Controller] Initializing...")
+
+	LevelService = self:GetService("LevelService")
 
 	self:DebugLog("[Level Controller] Initialized!")
 end
@@ -21,8 +47,42 @@ end
 -- @Description : Called after all controllers are loaded.
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 function LevelController:Start()
-
 	self:DebugLog("[Level Controller] Running!")
+
+	AvatarController = self:GetController("AvatarController")
+	LightingController = self:GetController("LightingController")
+
+	------------------------------------------------------
+	-- Registering lighting states with lighting system --
+	------------------------------------------------------
+	for _,LightingModule in pairs(LocalPlayer.PlayerScripts.Modules.LightingConfigs:GetChildren()) do
+		LightingController:RegisterLightingState(LightingModule.Name,require(LightingModule))
+	end
+
+	-------------------------------------------------------
+	-- Executing a level when it is loaded on the server --
+	-------------------------------------------------------
+	LevelService.LevelLoaded:connect(function(LevelName,Map)
+		local LevelConfig = require(LevelConfigs[LevelName])
+
+		LevelMusic_Sound.SoundId = "rbxassetid://" .. LevelConfig.MusicID
+		
+		LightingController:LoadLightingState(LevelConfig.LightingState)
+		LevelMusic_Sound:Play()
+
+		for _,Object in pairs(Map:GetDescendants()) do
+			if CollectionService:HasTag(Object,"Kill") then
+				Object.Touched:connect(function(TouchingPart)
+					if TouchingPart:IsDescendantOf(LocalPlayer.Character) then
+						LocalPlayer.Character.Humanoid.Health = 0
+						AvatarController:SetRagdolled(true)
+					end
+				end)
+			end
+		end
+	end)
+
+	LevelService:RunLevel("TestLevel1")
 end
 
 return LevelController
--- src/Server/Services/LevelService/init.lua
+++ src/Server/Services/LevelService/init.lua
@@ -7,12 +7,53 @@
 local LevelService = {Client = {}}
 LevelService.Client.Server = LevelService
 
+---------------------
+-- Roblox Services --
+---------------------
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Workspace = game:GetService("Workspace")
+local ServerStorage = game:GetService("ServerStorage")
+
+------------------
+-- Dependencies --
+------------------
+local AvatarService;
+
+-------------
+-- Defines --
+-------------
+local LevelConfigs = ReplicatedStorage.Modules.LevelConfigs
+local Maps = ServerStorage.Assets.Maps
+
+------------
+-- Events --
+------------
+local LevelLoaded; -- Fired to the client when a level is loaded
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- @Name : Client.RunLevel
+-- @Description : Runs the specified level
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+function LevelService.Client:RunLevel(Player,LevelName)
+	local LevelConfig = require(LevelConfigs[LevelName])
+	local Map = Maps[LevelConfig.Map]:Clone()
+
+	Map.Parent = Workspace
+	Map:MoveTo(Vector3.new(5000,0,0))
+	AvatarService:LoadPlayerCharacter(Player)
+	Player.Character:SetPrimaryPartCFrame(Map.Spawn.CFrame)
+
+	LevelLoaded:FireClient(Player,LevelName,Map)
+end
+
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- @Name : Init
 -- @Description : Called when the service module is first loaded.
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 function LevelService:Init()
 	self:DebugLog("[Level Service] Initializing...")
+
+	LevelLoaded = self:RegisterServiceClientEvent("LevelLoaded")
 
 	self:DebugLog("[Level Service] Initialized!")
 end
@@ -22,8 +63,9 @@ end
 -- @Description : Called after all services are loaded.
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 function LevelService:Start()
-
 	self:DebugLog("[Level Service] Running!")
+
+	AvatarService = self:GetService("AvatarService")
 end
 
 return LevelService
