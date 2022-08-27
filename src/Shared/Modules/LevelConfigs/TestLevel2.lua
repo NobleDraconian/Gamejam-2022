e3825f1a3ce5db12162114e232771160432ed893
Noble_Draconian <Binarybyte.DEV@gmail.com>
2022-08-27 19:06:32 -0400

    Fix config issue in test level 2

--- src/Shared/Modules/LevelConfigs/TestLevel2.lua
+++ src/Shared/Modules/LevelConfigs/TestLevel2.lua
@@ -2,7 +2,7 @@ local ServerStorage = game:GetService("ServerStorage")
 
 return {
 	Name = "Test Level 2",
-	Map = ServerStorage.Maps.TestLevel2,
+	Map = "TestLevel2",
 	LightingState = "Test2",
 	MusicID = "1840986188"
 }
