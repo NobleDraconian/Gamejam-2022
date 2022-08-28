f3ce66e67c72049a04085332332ad7cdcbed7564
Noble_Draconian <Binarybyte.DEV@gmail.com>
2022-08-28 03:53:20 -0400

    Implement initial one-off item interactions

--- src/Client/Controllers/ItemController/init.lua
+++ src/Client/Controllers/ItemController/init.lua
@@ -16,24 +16,33 @@ local Players = game:GetService("Players")
 -- Events --
 ------------
 local ItemUsed; -- Fired when the player left clicks while holding an item
+local ItemEquipped; -- Fired when the player equips an item
 
 -------------
 -- Defines --
 -------------
 local LocalPlayer = Players.LocalPlayer
+local TriggerDebounce = false
+local ChildAdded_Connection;
 
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- Helper functions
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 local function HandleLeftClick(_,UserInputState)
 	if UserInputState == Enum.UserInputState.Begin then
-		if LocalPlayer.Character ~= nil then
-			for _,Object in pairs(LocalPlayer.Character:GetChildren()) do
-				if Object:IsA("Tool") then
-					warn(Object.Name)
-					ItemUsed:Fire(Object.Name)
+		if not TriggerDebounce then
+			TriggerDebounce = true
+
+			if LocalPlayer.Character ~= nil then
+				for _,Object in pairs(LocalPlayer.Character:GetChildren()) do
+					if Object:IsA("Tool") then
+						warn(Object.Name)
+						ItemUsed:Fire(Object.Name,Object)
+					end
 				end
 			end
+			task.wait(2)
+			TriggerDebounce = false
 		end
 	end
 end
@@ -46,6 +55,7 @@ function ItemController:Init()
 	self:DebugLog("[Item Controller] Initializing...")
 
 	ItemUsed = self:RegisterControllerClientEvent("ItemUsed")
+	ItemEquipped = self:RegisterControllerClientEvent("ItemEquipped")
 	
 	self:DebugLog("[Item Controller] Initialized!")
 end
@@ -58,6 +68,21 @@ function ItemController:Start()
 	self:DebugLog("[Item Controller] Running!")
 
 	ContextActionService:BindAction("UseHelditem",HandleLeftClick,true,Enum.UserInputType.MouseButton1)
+
+	LocalPlayer.CharacterAdded:connect(function(Character)
+		if ChildAdded_Connection ~= nil then
+			if ChildAdded_Connection.Connected then
+				ChildAdded_Connection:Disconnect()
+			end
+		end
+
+		ChildAdded_Connection = Character.ChildAdded:connect(function(Object)
+			if Object:IsA("Tool") then
+				warn(Object.Name)
+				ItemEquipped:Fire(Object.Name,Object)
+			end
+		end)
+	end)
 end
 
 return ItemController
--- src/Client/Controllers/LevelController/init.lua
+++ src/Client/Controllers/LevelController/init.lua
@@ -12,6 +12,7 @@ local LevelController = {}
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local CollectionService = game:GetService("CollectionService")
 local Players = game:GetService("Players")
+local Workspace = game:GetService("Workspace")
 
 ------------------
 -- Dependencies --
@@ -25,10 +26,25 @@ local TransitionUI = require(script.TransitionUI)
 -------------
 -- Defines --
 -------------
+local BASE_STATE = {
+	Area1 = {
+		SaplingFertilized = false,
+		SaplingWatered = false,
+		SaplingIsGrown = false,
+		GrownSaplingChopped = false
+	},
+	Area2 = {
+		StickLit = false,
+		TreesLit = false
+	}
+}
+
 local LocalPlayer = Players.LocalPlayer
 local LevelConfigs = ReplicatedStorage.Modules.LevelConfigs
 local IsInFuture = false
+local CurrentState = {}
 
+local CurrentMap;
 local LevelMusic_Sound = Instance.new('Sound')
 LevelMusic_Sound.Parent = script
 LevelMusic_Sound.Looped = true
@@ -42,6 +58,8 @@ local function ShowModel(Model)
 			Object.Transparency = Object:GetAttribute("OriginalTransparency")
 			Object.CanCollide = Object:GetAttribute("OriginalCanCollide")
 			Object.CanTouch = Object:GetAttribute("OriginalCanTouch")
+		elseif Object:IsA("ParticleEmitter") then
+			Object.Enabled = true
 		end
 	end
 end
@@ -56,8 +74,29 @@ local function HideModel(Model)
 			Object.Transparency = 1
 			Object.CanCollide = false
 			Object.CanTouch = false
+		elseif Object:IsA("ParticleEmitter") then
+			Object.Enabled = false
 		end
 	end
+end
+
+local function GetPartWithTag(Tag)
+	for _,Object in pairs(Workspace:GetDescendants()) do
+		if CollectionService:HasTag(Object,Tag) then
+			return Object
+		end
+	end
+end
+
+local function CopyTable(original)
+	local copy = {}
+	for k, v in pairs(original) do
+		if type(v) == "table" then
+			v = CopyTable(v)
+		end
+		copy[k] = v
+	end
+	return copy
 end
 
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -104,9 +143,13 @@ function LevelController:Start()
 	-- Executing a level when it is loaded on the server --
 	-------------------------------------------------------
 	LevelService.LevelLoaded:connect(function(LevelName,Map)
+		warn("LEVEL LOADED")
 		local LevelConfig = require(LevelConfigs[LevelName])
 		local FinishTouched_Connection;
 		local DeathTouched_Connection;
+		CurrentMap = Map
+		IsInFuture = false
+		CurrentState = CopyTable(BASE_STATE)
 
 		LevelMusic_Sound.SoundId = "rbxassetid://" .. LevelConfig.MusicID
 		
@@ -116,7 +159,10 @@ function LevelController:Start()
 		for _,ShiftableModel in pairs(CollectionService:GetTagged("TimeShiftable")) do
 			HideModel(ShiftableModel.Future)
 			HideModel(ShiftableModel.Present)
-			ShowModel(ShiftableModel.Present)
+
+			if ShiftableModel:GetAttribute("Visible") ~= false then
+				ShowModel(ShiftableModel.Present)
+			end
 		end
 		TransitionUI:Hide()
 
@@ -147,21 +193,164 @@ function LevelController:Start()
 		end)
 	end)
 
-	--------------------------
-	-- Handling time shifts --
-	--------------------------
+	----------------------------
+	-- Handling gear triggers --
+	----------------------------
 	ItemController.ItemUsed:connect(function(ItemName)
 		if ItemName == "TimeLantern" then
 			IsInFuture = not IsInFuture
 
 			for _,ShiftableModel in pairs(CollectionService:GetTagged("TimeShiftable")) do
-				if IsInFuture then
-					ShowModel(ShiftableModel.Future)
-					HideModel(ShiftableModel.Present)
-				else
-					ShowModel(ShiftableModel.Present)
-					HideModel(ShiftableModel.Future)
+				if ShiftableModel:GetAttribute("Visible") ~= false then
+					if IsInFuture then
+						ShowModel(ShiftableModel.Future)
+						HideModel(ShiftableModel.Present)
+					else
+						ShowModel(ShiftableModel.Present)
+						HideModel(ShiftableModel.Future)
+					end
 				end
+			end
+		end
+	end)
+
+	ItemController.ItemEquipped:connect(function(ItemName,Item)
+		if ItemName == "FertilizerBag" then
+			if not CurrentState.Area1.SaplingFertilized and not IsInFuture then
+				local FertilizePart = GetPartWithTag("L1A1_Sapling")
+				local OldPrompt = FertilizePart:FindFirstChildWhichIsA("ProximityPrompt")
+				local ProximityPrompt = Instance.new('ProximityPrompt')
+				ProximityPrompt.ActionText = "Fertilize the Sapling"
+				ProximityPrompt.Parent = FertilizePart
+				local PromptTriggered_Connection;
+
+				if OldPrompt ~= nil then
+					OldPrompt:Destroy()
+				end
+
+				PromptTriggered_Connection = ProximityPrompt.Triggered:connect(function()
+					warn("TRIGGERED")
+					PromptTriggered_Connection:Disconnect()
+					ProximityPrompt:Destroy()
+					Item:Destroy()
+
+					CurrentState.Area1.SaplingFertilized = true
+
+					if CurrentState.Area1.SaplingFertilized and CurrentState.Area1.SaplingWatered and not CurrentState.Area1.SaplingIsGrown then
+						CurrentState.Area1.SaplingIsGrown = true
+						HideModel(CurrentMap.Course.Area1.Sapling_BadOutcome)
+						ShowModel(CurrentMap.Course.Area1.Sapling_GoodOutcome.Present)
+						CurrentMap.Course.Area1.Sapling_BadOutcome:SetAttribute("Visible",false)
+						CurrentMap.Course.Area1.Sapling_GoodOutcome:SetAttribute("Visible",true)
+					end
+				end)
+			end
+		elseif ItemName == "WateringCan" then
+			if not CurrentState.Area1.SaplingWatered and not IsInFuture then
+				local WaterPart = GetPartWithTag("L1A1_Sapling")
+				local OldPrompt = WaterPart:FindFirstChildWhichIsA("ProximityPrompt")
+				local ProximityPrompt = Instance.new('ProximityPrompt')
+				ProximityPrompt.ActionText = "Water the Sapling"
+				ProximityPrompt.Parent = WaterPart
+				local PromptTriggered_Connection;
+
+				if OldPrompt ~= nil then
+					OldPrompt:Destroy()
+				end
+
+				PromptTriggered_Connection = ProximityPrompt.Triggered:connect(function()
+					warn("TRIGGERED")
+					PromptTriggered_Connection:Disconnect()
+					ProximityPrompt:Destroy()
+					Item:Destroy()
+
+					CurrentState.Area1.SaplingWatered = true
+
+					if CurrentState.Area1.SaplingFertilized and CurrentState.Area1.SaplingWatered and not CurrentState.Area1.SaplingIsGrown then
+						CurrentState.Area1.SaplingIsGrown = true
+						HideModel(CurrentMap.Course.Area1.Sapling_BadOutcome)
+						ShowModel(CurrentMap.Course.Area1.Sapling_GoodOutcome.Present)
+						CurrentMap.Course.Area1.Sapling_BadOutcome:SetAttribute("Visible",false)
+						CurrentMap.Course.Area1.Sapling_GoodOutcome:SetAttribute("Visible",true)
+					end
+				end)
+			end
+		elseif ItemName == "Axe" then
+			warn(CurrentState)
+			if CurrentState.Area1.SaplingIsGrown and IsInFuture then
+				local AxePart = GetPartWithTag("L1A1_Sapling")
+				local OldPrompt = AxePart:FindFirstChildWhichIsA("ProximityPrompt")
+				local ProximityPrompt = Instance.new('ProximityPrompt')
+				ProximityPrompt.ActionText = "Chop the tree"
+				ProximityPrompt.RequiresLineOfSight = false
+				ProximityPrompt.Parent = AxePart
+				local PromptTriggered_Connection;
+
+				if OldPrompt ~= nil then
+					OldPrompt:Destroy()
+				end
+
+				PromptTriggered_Connection = ProximityPrompt.Triggered:connect(function()
+					warn("TRIGGERED")
+					PromptTriggered_Connection:Disconnect()
+					ProximityPrompt:Destroy()
+					Item:Destroy()
+
+					CurrentState.Area1.GrownSaplingChopped = true
+				end)
+			end
+		elseif ItemName == "Stick" then
+			if not CurrentState.Area2.StickLit and not IsInFuture then
+				local FirePart = GetPartWithTag("L2A2_Campfire")
+				local OldPrompt = FirePart:FindFirstChildWhichIsA("ProximityPrompt")
+				local ProximityPrompt = Instance.new('ProximityPrompt')
+				ProximityPrompt.ActionText = "Set on fire"
+				ProximityPrompt.RequiresLineOfSight = false
+				ProximityPrompt.Parent = FirePart
+				local PromptTriggered_Connection;
+
+				if OldPrompt ~= nil then
+					OldPrompt:Destroy()
+				end
+
+				PromptTriggered_Connection = ProximityPrompt.Triggered:connect(function()
+					warn("TRIGGERED")
+					PromptTriggered_Connection:Disconnect()
+					ProximityPrompt:Destroy()
+
+					Instance.new('Fire',Item.Handle)
+
+					CurrentState.Area2.StickLit = true
+				end)
+			end
+
+			if CurrentState.Area2.StickLit and (not CurrentState.Area2.TreesLit) and not IsInFuture then
+				local FirePart = GetPartWithTag("L2A2_FireTrees")
+				local OldPrompt = FirePart:FindFirstChildWhichIsA("ProximityPrompt")
+				local ProximityPrompt = Instance.new('ProximityPrompt')
+				ProximityPrompt.ActionText = "Set on fire"
+				ProximityPrompt.RequiresLineOfSight = false
+				ProximityPrompt.Parent = FirePart
+				local PromptTriggered_Connection;
+
+				if OldPrompt ~= nil then
+					OldPrompt:Destroy()
+				end
+
+				PromptTriggered_Connection = ProximityPrompt.Triggered:connect(function()
+					warn("TRIGGERED")
+					PromptTriggered_Connection:Disconnect()
+					ProximityPrompt:Destroy()
+					Item:Destroy()
+
+					ShowModel(CurrentMap.Course.Area2.BlockingTrees_Fire.Present)
+					HideModel(CurrentMap.Course.Area2.BlockingTrees_NoFire)
+
+					CurrentMap.Course.Area2.BlockingTrees_Fire:SetAttribute("Visible",true)
+					CurrentMap.Course.Area2.BlockingTrees_NoFire:SetAttribute("Visible",false)
+
+					CurrentState.Area2.TreesLit = true
+				end)
 			end
 		end
 	end)
